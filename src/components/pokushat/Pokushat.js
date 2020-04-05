c890d4700c9f3c94bf2c4a531552333cc2809572
Andrey Ermakov <a.ermakov@corp.vk.com>
2020-04-05 16:34:09 +0300

    add container to map

--- src/components/pokushat/Pokushat.js
+++ src/components/pokushat/Pokushat.js
@@ -72,7 +72,8 @@ export default class App extends React.Component {
     return (
       <div className="App">
         <div className="layer">
-          <YMaps query={{ apikey: 'a37ce737-cfc2-4b5c-b93a-a0b0b21800cd' }}>
+          <div style={{height:"70vh", width:"100%"}}>
+            <YMaps query={{ apikey: 'a37ce737-cfc2-4b5c-b93a-a0b0b21800cd' }}>
             <Map
               state={mapState}
               instanceRef={(ref) => (this.map = ref)}
@@ -88,6 +89,7 @@ export default class App extends React.Component {
               </Clusterer>
             </Map>
           </YMaps>
+          </div>
           <Button disabled={!ready} my={1} primary emphasized onClick={this.findQushat}>
             Кушац
           </Button>
